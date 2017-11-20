c44ca6cb0942a57a5d4188420f4ad2df0bc79119
Benoît Sauvage <benoit.sauvage44@gmail.com>
2017-11-20 10:53:49 -0500

    [FEAT] Leaderboard activity

---
+++ app/src/main/java/fr/benoitsauvage/game/ScoresActivity.java
@@ -0,0 +1,65 @@
+package fr.benoitsauvage.game;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Map;
+
+public class ScoresActivity extends Activity {
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+
+        sharedPref = this.getSharedPreferences("scores", Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+        LinearLayout layout = findViewById(R.id.scores_layout);
+
+        Map<String, ?> allEntries = sharedPref.getAll();
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            Log.d("SCORES", entry.getKey() + ": " + entry.getValue().toString());
+            layout.addView(generateLayout(entry));
+        }
+
+        Button clear = findViewById(R.id.scores_clear);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.clear().apply();
+                finish();
+            }
+        });
+    }
+
+    private LinearLayout generateLayout(final Map.Entry<String, ?> entry) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.score_layout, null, false);
+
+        TextView score_name = layout.findViewById(R.id.score_name);
+        TextView score = layout.findViewById(R.id.score_score);
+
+        score_name.setTypeface(Typeface.MONOSPACE);
+        score_name.setText(entry.getKey());
+
+        score.setTypeface(Typeface.MONOSPACE);
+        score.setText(entry.getValue().toString());
+
+        return layout;
+    }
+}
